e65452dcec17e7901e899508346b19bb1ef93b08
Kyle Geske <stungeye@gmail.com>
2024-09-16 16:46:28 -0500

    Adding in the actual models names.

--- src/classes/apiProviders.ts
+++ src/classes/apiProviders.ts
@@ -11,13 +11,19 @@ export const ApiModels = {
     { name: "GPT-4o mini", value: "gpt-4o-mini" },
     { name: "GPT-3.5 Turbo", value: "gpt-3.5" },
   ],
-  [ApiProviders.Groq]: [{ name: "Groq Model 1", value: "groq-model-1" }],
+  [ApiProviders.Groq]: [
+    { name: "Groq Llama 3.1 70b Versatile", value: "llama-3.1-70b-versatile" },
+    { name: "Groq Llama 3.1 8b Instant", value: "llama-3.1-8b-instant" },
+  ],
   [ApiProviders.Google]: [
-    { name: "Gemini Model 1", value: "gemini-1" },
-    { name: "Gemini Model 2", value: "gemini-2" },
+    { name: "Gemini 1.5 Flash", value: "gemini-1.5-flash" },
+    { name: "Gemini 1.5 Pro", value: "gemini-1.5-pro" },
   ],
   [ApiProviders.Anthropic]: [
-    { name: "Claude v1", value: "claude-v1" },
-    { name: "Claude v2", value: "claude-v2" },
+    { name: "Claude 3.5 Sonnet", value: "claude-3-5-sonnet-20240620" },
+    { name: "Claude 3 Opus", value: "claude-3-opus-20240229" },
+    { name: "Claude 3 Haiku", value: "claude-3-haiku-20240307" },
   ],
 } as const;
+
+export type ApiModel = (typeof ApiModels)[ApiProviders][number]["value"];
